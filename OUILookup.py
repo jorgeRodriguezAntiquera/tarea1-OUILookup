34bfb705e72cef2cb1c5631f78902c0d11f36cdb
jorge <jorge.rodrigeza@alumnos.uv.cl>
2020-11-20 17:13:03 -0300

    Implementacion

--- OUILookup.py
+++ OUILookup.py
@@ -40,18 +40,10 @@ def main():
             leer(MAC)
         
             
-    #RATE y MAX_TIME deben estar ingresadas.
+    #MAC y IP deben estar ingresadas.
     if (MAC == None and IP == None):
       print ("Error: Faltan parametros obligatorios.")
       uso()
-
-    
-    
-    #Ahora se pasa al programa
-    #eventos = []
-    #genEvents(RATE, eventos)
-    
-    #showEvents(eventos)
 
 #
 #
@@ -73,28 +65,16 @@ def extraccion():
   with open(filename, 'wb') as fd:
     for chunk in response.iter_content(chunk_size):
         fd.write(chunk)
- # leer('00:00:00')
+ 
 
 def macAddres(IP):
- # encoding = 'utf-8'
- # IP = "192.168.1.30"
- # pid = Popen(["arp", "-n", IP], stdout=PIPE)
- # s = pid.communicate()[0]
- # MAC = re.search(r"(([a-f\d]{1,2}\:){5}[a-f\d]{1,2})", s).groups()[0]
- # MAC.decode(encoding)
- # leer(MAC)
   nombre_equipo = socket.gethostname()
   direccion_equipo = socket.gethostbyname(nombre_equipo)
   if(direccion_equipo == IP):
-#  print ('La IP es: ' + direccion_equipo)
     mac = get_mac()
     s = '{0:16x}'.format(mac)
     s = ':'.join(re.findall(r'\w\w', s))
-    leer (s)
-#  print (mac)
-    
-#  print(s)
-    
+    leer (s) 
   else:
     print ("Error: ip is outside the host network")
 
@@ -103,14 +83,6 @@ def macAddres(IP):
 def leer(MAC):
   enc='utf-8'
   line=[67]
-  #i=0
-  #f=open('test.txt')
-  #for line in f:
-  #  #print (line)
-  #   i+=1  
-#busca RATE(mac) en el test.txt y lo almacena en macs[]
-  #MAC = input()
- # MAC = '00:00:00'
   macs = []
   with open('test.txt','r', encoding=enc) as lineas:
     for line in lineas:
@@ -130,12 +102,12 @@ def leer(MAC):
  
 
 #
-# Genera Eventos
+# Direccion MAC o IP
 # Entrada: 
-#          rate: tasa de arribo de paquetes.
-#          maxTime: tiempo maximo de generacion.
+#          MAC: tasa de arribo de paquetes.
+#          IP: tiempo maximo de generacion.
 # Salida:
-#          eventos : lista que contiene los tiempos de los eventos generados.
+#          Mostrar MAC y Vendor
 #
 
 
